db459c5d42b1e828cf781d040dc089095515beea
James Pants <jamespants@gmail.com>
2015-04-20 12:54:36 +0200

    css swaps

--- nyc-festival-custom-tag-page.js
+++ nyc-festival-custom-tag-page.js
@@ -6,14 +6,56 @@
 
 
 function changeStyles(){
+	var blue = '#00283e';
+	var pink = '#e71680';
 	var tagPage = $('.tag-page');
+	var div = $('.article');
+	var text = div.find('.text-content');
 
+	
+	// ------------------------------------------------
+	// Change background of whole page
+	//
 	tagPage.css({
-		background: 'blue'
+		background: blue
 	});
+
+	// ------------------------------------------------
+	// Change background of individual recommendation
+	//
+	
+	div.css({
+		background: blue
+	});
+
+
+	
+	// ------------------------------------------------
+	// Change text headers
+	//
+	
+	text.find('h3').css({
+		color: 'white'
+	});
+
+	text.find('p').css({
+		color: 'white'
+	});
+
+
+	
+
+	
 }
 
 
+
+
+// -------------------------------------------------
+//
+// Load
+// 
+// -------------------------------------------------
 
 $(document).ready(function(){
 	changeStyles();

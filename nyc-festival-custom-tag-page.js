02decd52fa3a62016261be60601089ec4b69a109
James Pants <jamespants@gmail.com>
2015-04-20 12:26:29 +0200

    jq

--- nyc-festival-custom-tag-page.js
+++ nyc-festival-custom-tag-page.js
@@ -1,20 +1,23 @@
 
 'use strict';
 
+/*global $:false */
+
 
 
 function changeStyles(){
-	var tagPage = document.getElementById('page');
-	console.log('Loaded');
-	tagPage.classList.remove('tag-page');
-	tagPage.style.background = 'blue';
+	var tagPage = $('.tag-page');
+
+	tagPage.css({
+		background: 'blue'
+	});
 }
 
 
 
-document.onload = function(){
-	setTimeout(changeStyles, 2000);
-};
+$(document).ready(function(){
+	changeStyles();
+});
 
 
 
